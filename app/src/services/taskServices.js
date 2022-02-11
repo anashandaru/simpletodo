7010aadd537fb369ddff200025ba22162c0585a6
PajarKharisma <pajarkharisma@hotmail.com>
2022-02-11 20:57:17 +0700

    pajar - update

---
+++ app/src/services/taskServices.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+const apiUrl = "http://localhost:8080/api/tasks";
+
+export function postFilter(filter){
+    let task;
+    filter = parseInt(filter);
+    switch (filter) {
+        case 0:
+            task = {};
+            break;
+        case 1:
+            task = {completed: true};
+            break;
+        case 2:
+            task = {completed: false};
+            break;
+        default:
+            break;
+    }
+    return axios.post(`${apiUrl}/${filter}`, task);
+}
+
+export function getTasks() {
+    return axios.get(apiUrl);
+}
+
+export function addTask(task) {
+    return axios.post(apiUrl, task);
+}
+
+export function updateTask(id, task) {
+    return axios.put(apiUrl + "/" + id, task);
+}
+
+export function deleteTask(id) {
+    return axios.delete(apiUrl + "/" + id);
+}
