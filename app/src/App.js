84d4969f19334223ba6e62a6c4f44af079874233
anashandaru <anas.s.handaru@gmail.com>
2022-02-14 14:54:14 +0700

    implement router app.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,22 @@
 import TaskApp from "./pages/Task";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
-    return(
-        <TaskApp/>
-    )
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<TaskApp/>}/>
+            <Route path="login" element={<Login/>}/>
+            <Route path="register" element={<Register/>}/>
+        </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
