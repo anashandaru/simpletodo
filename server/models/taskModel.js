f635fffd74f9b450e655ce0f3b9e9bd9d9b3db4b
anashandaru <anas.s.handaru@gmail.com>
2022-02-15 10:12:02 +0700

    add relationship between db models

--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const taskSchema = mongoose.Schema({
-    content: { type: String, required: true },
-    completed: { type: Boolean, required: true, default: false },
-    created_date: { type: Date, default: Date.now() }
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  content: { type: String, required: true },
+  completed: { type: Boolean, required: true, default: false },
+  created_date: { type: Date, default: Date.now() },
 });
 
-module.exports = mongoose.model('Task', taskSchema);
+module.exports = mongoose.model("Task", taskSchema);
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,27 +1,30 @@
 const mongoose = require("mongoose");
 
 const userScheme = new mongoose.Schema({
-    first_name: {
-        type: String,
-        default: null
+  first_name: {
+    type: String,
+    default: null,
+  },
+  last_name: {
+    type: String,
+    default: null,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  role: [
+    {
+      type: String,
     },
-    last_name: {
-        type: String,
-        default: null 
-    },
-    email: {
-        type: String,
-        unique: true
-    },
-    password: {
-        type: String 
-    },
-    role: [{
-            type: String
-        }],
-    token: { 
-        type: String 
-    },
+  ],
+  token: {
+    type: String,
+  },
+  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
 });
 
 module.exports = mongoose.model("User", userScheme, "user");
