53be474efdc5ed260b02b1002b1df0e417932977
PajarKharisma <pajarkharisma@hotmail.com>
2022-02-15 09:02:21 +0700

    change Register to function component

--- app/src/pages/Register.js
+++ app/src/pages/Register.js
@@ -1,92 +1,98 @@
 import { Paper, Button, TextField } from "@material-ui/core";
-import { Component } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate  } from 'react-router-dom';
 import "../App.css";
 
 import { register } from "../services/authServices";
 
-class Register extends Component{
-  state = {
-    first_name: "",
-    last_name: "",
-    email: "",
-    password: "",
-    role:["admin"],
-    redirect: false
-  };
+const Register = (props) => {
+  const [first_name, setFirstname] = useState("");
+  const [last_name, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState([]);
 
-  handeChange = (event) => {
-      this.setState({
-          [event.target.name] : event.target.value
-      });
-  };
+  const navigate = useNavigate();
 
-  handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        const { data } = await register(this.state);
+        const { data } = await register({
+          first_name,
+          last_name,
+          email,
+          password,
+          role
+        });
         console.log(data);
         localStorage.setItem("token", data.token);
+        navigate('/');
     } catch (error) {
         console.log(error);
+        navigate('/login');
     }
   };
 
-  render(){
-    return (
-      <div className="App flex">
-        <Paper className="container">
-          <div className="heading">Register</div>
-          <form>
-            <br />
-            <TextField
-              name="first_name"
-              onChange={this.handeChange}
-              variant="outlined"
-              size="small"
-              style={{ width: "100%" }}
-              label="first name"
-            />
-            <br />
-            <br />
-            <TextField
-              name="last_name"
-              onChange={this.handeChange}
-              variant="outlined"
-              size="small"
-              style={{ width: "100%" }}
-              label="last name"
-            />
-            <br />
-            <br />
-            <TextField
-              name="email"
-              onChange={this.handeChange}
-              variant="outlined"
-              size="small"
-              style={{ width: "100%" }}
-              label="email"
-            />
-            <br />
-            <br />
-            <TextField
-              name="password"
-              onChange={this.handeChange}
-              variant="outlined"
-              size="small"
-              style={{ width: "100%" }}
-              label="password"
-              type="password"
-            />
-            <br />
-            <br />
-            <Button type="submit" onClick={this.handleSubmit} color="primary" variant="contained">
-              Register
-            </Button>
-          </form>
-        </Paper>
-      </div>
-    );   
-  }
+  useEffect(() => {
+    if(localStorage.getItem('token')) {
+        navigate('/');
+      }
+  }, []);
+
+  return (
+    <div className="App flex">
+      <Paper className="container">
+        <div className="heading">Register</div>
+        <form>
+          <br />
+          <TextField
+            name="first_name"
+            onChange={setFirstname}
+            variant="outlined"
+            size="small"
+            style={{ width: "100%" }}
+            label="first name"
+          />
+          <br />
+          <br />
+          <TextField
+            name="last_name"
+            onChange={setLastname}
+            variant="outlined"
+            size="small"
+            style={{ width: "100%" }}
+            label="last name"
+          />
+          <br />
+          <br />
+          <TextField
+            name="email"
+            onChange={setEmail}
+            variant="outlined"
+            size="small"
+            style={{ width: "100%" }}
+            label="email"
+          />
+          <br />
+          <br />
+          <TextField
+            name="password"
+            onChange={setPassword}
+            variant="outlined"
+            size="small"
+            style={{ width: "100%" }}
+            label="password"
+            type="password"
+          />
+          <br />
+          <br />
+          <Button type="submit" onClick={handleSubmit} color="primary" variant="contained">
+            Register
+          </Button>
+        </form>
+      </Paper>
+    </div>
+  );
 }
 
 export default Register;
