2b178f6aec64297c0ea0fd6f2b948bdc74a5411a
anashandaru <anas.s.handaru@gmail.com>
2022-02-17 10:01:56 +0700

    Verify user input on registration API

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,74 +2,86 @@
 const bcrypt = require("bcryptjs/dist/bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    register : async (req, res, next) => {
-        try{
-            const { first_name, last_name, email, password, role } = req.body;
-            console.log(req.body);
+  register: async (req, res, next) => {
+    try {
+      const { first_name, last_name, email, password, role } = req.body;
+      email = email.toLowerCase();
 
-            if(!(email && password && first_name && last_name && role)){
-                return res.status(404).send("All input is required");
-            }
+      if (!(email && password && first_name && last_name && role)) {
+        return res.status(404).send("All input is required");
+      }
 
-            const oldUser = await User.findOne({email});
+      if (password.length < 8) {
+        return res.status(404).send("Password should more than 8 characters");
+      }
 
-            if(oldUser){
-                return res.status(409).send("User already exist. Please login.");
-            }
+      if (!email.includes("@")){
+          return res.status(404).send("Please enter valid email address");
+      }
 
-            encryptedPassword = await bcrypt.hash(password, 10);
+      if (first_name.trim().length === 0 || last_name.trim().length){
+          return res.status(404).send("Please enter valid input");
+      }
 
-            const user = await User.create({
-                first_name,
-                last_name,
-                role,
-                email: email.toLowerCase(),
-                password: encryptedPassword,
-            });
+      const oldUser = await User.findOne({ email });
 
-            const token = jwt.sign(
-                { user_id: user._id, email },
-                process.env.TOKEN_KEY,
-                {
-                    expiresIn: "2h",
-                }
-            );
+      if (oldUser) {
+        return res.status(409).send("User already exist. Please login.");
+      }
 
-            user.token = token;
-            return res.status(201).json(user);
-        } catch (err){
-            return res.json(err);
+      encryptedPassword = await bcrypt.hash(password, 10);
+
+      const user = await User.create({
+        first_name,
+        last_name,
+        role,
+        email: email.toLowerCase(),
+        password: encryptedPassword,
+      });
+
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
         }
-    },
+      );
 
-    login: async (req, res, next) => {
-        try{
-            const { email, password } = req.body;
+      user.token = token;
+      return res.status(201).json(user);
+    } catch (err) {
+      return res.json(err);
+    }
+  },
 
-            if(!(email && password)) {
-                return res.status(404).send("All input is required");
-            }
+  login: async (req, res, next) => {
+    try {
+      const { email, password } = req.body;
 
-            const user = await User.findOne({email});
-            if(user && (await bcrypt.compare(password, user.password))){
-                const token = jwt.sign(
-                    { user_id: user._id, email },
-                    process.env.TOKEN_KEY,
-                    {
-                        expiresIn: "2h"
-                    }
-                );
+      if (!(email && password)) {
+        return res.status(404).send("All input is required");
+      }
 
-                user.token = token;
-                return res.status(200).json(user);
-            }
+      const user = await User.findOne({ email });
+      if (user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign(
+          { user_id: user._id, email },
+          process.env.TOKEN_KEY,
+          {
+            expiresIn: "2h",
+          }
+        );
 
-            return res.status(500).send("User not found");
-        } catch (error){
-            return res.json(error);
-        }
-    },
+        user.token = token;
+        return res.status(200).json(user);
+      }
+
+      return res.status(500).send("User not found");
+    } catch (error) {
+      return res.json(error);
+    }
+  },
 };
